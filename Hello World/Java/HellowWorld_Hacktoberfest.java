0aea4cb2b1db57393c1da8ca4803becae25970d8
Juny <juny.santeliz@gmail.com>
2019-10-19 15:00:24 -0400

    HellowWorld in Java for hacktoberfest challenge

---
+++ Hello World/Java/HellowWorld_Hacktoberfest.java
@@ -0,0 +1,10 @@
+public class HellowWorld_Hacktoberfest {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println();
+        System.out.println("This was made using VS Code!");
+    }
+
+}
+
